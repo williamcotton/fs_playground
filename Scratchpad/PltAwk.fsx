6479c69685308acbfafba765e7f6f89aca2965f6
William Cotton <williamc@eurekasoft.com>
2023-11-09 08:08:29 -0600

    clean up some code

--- Scratchpad/PltAwk.fsx
+++ Scratchpad/PltAwk.fsx
@@ -1,20 +1,19 @@
 #!/usr/bin/env dotnet fsi
 
 #load "Unix.fsx"
-
 open Unix
 
 Ok "pattern
     runner
-    fatter"
+    batter"
     |> grep "tt"
-    |> awk "'{print $1 $1 $1}'"
+    |> awk """'{print $1 " " $1 " " $1}'"""
     |> echo
 
 Ok "one,two,three,date
     1,2,3,2021-01
     4,5,6,2021-02
-    7,8,9,2021-03"
+    7,8,2,2021-03"
     |> zsh "
         plt '[one, two, three], date { bar 10px [solid red, solid green, solid blue] }' |
         imgcat"
