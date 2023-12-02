f87bddf2f67261ed5c4b19035f2490e9d44d4d86
William Cotton <williamcotton@gmail.com>
2023-12-02 10:53:43 -0600

    aoc23: day 2, code golf

--- advent23/Day2.fsx
+++ advent23/Day2.fsx
@@ -12,33 +12,37 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
 
 let maxColors game = 
-  game |> List.fold (fun (r, g, b) (color, count) ->
-  match color with
-  | "red" -> (max r count, g, b)
-  | "green" -> (r, max g count, b)
-  | "blue" -> (r, g, max b count)
-  | _ -> (r, g, b)) (0, 0, 0)
+    game |> List.fold (fun (r, g, b) (color, count) ->
+    match color with
+    | "red" -> (max r count, g, b)
+    | "green" -> (r, max g count, b)
+    | "blue" -> (r, g, max b count)
+    | _ -> (r, g, b)) (0, 0, 0)
+
+let validGame maxColor =
+    let (maxRed, maxGreen, maxBlue) = maxColor
+    maxRed <= 12 && maxGreen <= 13 && maxBlue <= 14
 
 let games = 
     text.Split('\n')
-      |> Array.map (fun s -> Regex(@"(?<count>\d+) (?<color>red|green|blue)").Matches(s)) 
-      |> Array.map (fun m -> m |> Seq.map (fun g -> (g.Groups.["color"].Value, int g.Groups.["count"].Value)) |> Seq.toList)
-      |> Array.filter (fun game -> game.Length > 0)
+    |> Array.map (fun s -> Regex(@"(?<count>\d+) (?<color>red|green|blue)").Matches(s)) 
+    |> Array.map (fun m -> m |> Seq.map (fun g -> (g.Groups.["color"].Value, int g.Groups.["count"].Value)) |> Seq.toList)
+    |> Array.filter (fun game -> game.Length > 0)
 
-let a = games
-      |> Array.fold (fun (gameNumber, sumOfIdsOfGames) game ->
-          match maxColors game with
-          | (maxRed, maxGreen, maxBlue) when maxRed <= 12 && maxGreen <= 13 && maxBlue <= 14 -> 
-              (gameNumber + 1, sumOfIdsOfGames + gameNumber)
-          | _ -> 
-              (gameNumber + 1, sumOfIdsOfGames)
-        ) (1, 0)
-      |> snd
+let a =
+    games
+    |> Array.fold (fun (gameNumber, sumOfIdsOfGames) game ->
+        match maxColors game with
+        | maxColor when validGame maxColor -> (gameNumber + 1, sumOfIdsOfGames + gameNumber)
+        | _ -> (gameNumber + 1, sumOfIdsOfGames)
+      ) (1, 0)
+    |> snd
 
 printfn "%A" a
 
-let b = games
-      |> Array.map (fun game -> let (maxRed, maxGreen, maxBlue) = maxColors game in maxRed * maxGreen * maxBlue)
-      |> Array.sum
+let b = 
+    games
+    |> Array.map (fun game -> let (maxRed, maxGreen, maxBlue) = maxColors game in maxRed * maxGreen * maxBlue)
+    |> Array.sum
 
 printfn "%A" b
